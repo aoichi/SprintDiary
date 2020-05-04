1fe93eafbc1cda1ce4590026ab45d96f39b00628
aoichi <sososososochan@gmail.com>
2020-05-04 19:56:51 +0900

    見た目の変更と検索フォーム追加

--- diary/static/diary/script.js
+++ diary/static/diary/script.js
@@ -1,2 +1,13 @@
 
 $(".dropdown-trigger").dropdown();
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('#sidenav-2').sidenav({ edge: 'left' });
+  });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.sidenav');
+      var instances = M.Sidenav.init(elems, {});
+    });
--- diary/templatetags/diary.py
+++ diary/templatetags/diary.py
@@ -170,3 +170,10 @@ def render_search_form(request):
         form = DiarySearchForm(request.GET)
 
     return {'search_form': form}
+
+@register.simple_tag
+def url_replace(request, field, value):
+    """GETパラメータの一部を置き換える。"""
+    url_dict = request.GET.copy()
+    url_dict[field] = str(value)
+    return url_dict.urlencode()
